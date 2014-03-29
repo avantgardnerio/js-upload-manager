1a59448f51ae16cf34a9c65eebf1308f2941f388
Brent Gardner <bgardner@squarelabs.net>
2014-03-29 16:37:31 -0600

    Added image rescaling

--- scripts/UploadManager.js
+++ scripts/UploadManager.js
@@ -1,9 +1,16 @@
 var UploadManager = function() {
     var self = {};
 
-    // Constants
+    // Upload constants
     var CHUNK_SIZE = 20 * 1024;     // Set as needed according to bandwidth & latency
     var POLL_INTERVAL = 10;        // 250 for testing, lower for faster transfers
+
+    // Graphics constants
+    var TARGET_WIDTH = 800;
+    var TARGET_HEIGHT = 600;
+    var TARGET_FORMAT = 'image/jpeg';
+    var URL_PREFIX = 'data:' + TARGET_FORMAT + ';base64,';
+    var TARGET_QUALITY = 0.95;
 
     var state = new UploadState();
 
@@ -21,7 +28,38 @@ var UploadManager = function() {
         // Read the file from the disk
         var reader = new FileReader();
         reader.onload = function(ev) {
-            state.setData(ev.target.result);
+            var b64imgData = exports.encode(ev.target.result);
+            var img = new Image();
+            img.onload = function() {
+                var canvas = document.createElement('canvas');
+
+                // Calculate new size
+                var xRatio = TARGET_WIDTH / this.width;
+                var yRatio = TARGET_HEIGHT / this.height;
+                var ratio = Math.min(xRatio, yRatio);
+                var width = Math.round(this.width * ratio);
+                var height = Math.round(this.height * ratio);
+
+                // Resize the canvas
+                canvas.width = width;
+                canvas.height = height;
+                var ctx = canvas.getContext("2d");
+
+                // Draw the image
+                ctx.drawImage(img,
+                    0, 0, this.width, this.height,  // Source coords
+                    0, 0, width, height             // dest coords
+                );
+
+                // Grab a screenshot
+                var url = canvas.toDataURL(TARGET_FORMAT, TARGET_QUALITY);
+                b64imgData = url.substr(URL_PREFIX.length);
+                var byteArray = exports.decode(b64imgData);
+
+                // Upload!
+                state.setData(byteArray)
+            };
+            img.src = URL_PREFIX + b64imgData;
         };
         reader.readAsArrayBuffer(file);
     };
