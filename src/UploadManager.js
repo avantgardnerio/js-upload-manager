9a61afa66d9ecd55254c37e52614c39e0eda832c
Brent Gardner <bgardner@squarelabs.net>
2014-03-30 17:00:16 -0600

    Migrating sizing into filter - seems to fix test issue

--- src/UploadManager.js
+++ src/UploadManager.js
@@ -11,13 +11,6 @@ var UploadManager = function(FileReader, XMLHttpRequest) {
     // Upload constants
     var CHUNK_SIZE = 20 * 1024;     // Set as needed according to bandwidth & latency
 
-    // Graphics constants
-    var TARGET_WIDTH = 800;
-    var TARGET_HEIGHT = 600;
-    var TARGET_FORMAT = 'image/jpeg';
-    var URL_PREFIX = 'data:' + TARGET_FORMAT + ';base64,';
-    var TARGET_QUALITY = 0.95;
-
     var state = new UploadState();
 
     /**
@@ -34,39 +27,9 @@ var UploadManager = function(FileReader, XMLHttpRequest) {
         // Read the file from the disk
         var reader = new FileReader();
         reader.onload = function(ev) {
-            var b64imgData = Base64.encode(ev.target.result);
-            var img = new Image();
-            img.onload = function() {
-                var canvas = document.createElement('canvas');
-
-                // Calculate new size
-                var xRatio = TARGET_WIDTH / this.width;
-                var yRatio = TARGET_HEIGHT / this.height;
-                var ratio = Math.min(xRatio, yRatio);
-                var width = Math.round(this.width * ratio);
-                var height = Math.round(this.height * ratio);
-
-                // Resize the canvas
-                canvas.width = width;
-                canvas.height = height;
-                var ctx = canvas.getContext('2d');
-
-                // Draw the image
-                ctx.drawImage(img,
-                    0, 0, this.width, this.height,  // Source coords
-                    0, 0, width, height             // dest coords
-                );
-
-                // Grab a screenshot
-                var url = canvas.toDataURL(TARGET_FORMAT, TARGET_QUALITY);
-                b64imgData = url.substr(URL_PREFIX.length);
-                var byteArray = Base64.decode(b64imgData);
-
-                // Upload!
-                state.setData(byteArray);
-                uploadChunk();
-            };
-            img.src = URL_PREFIX + b64imgData;
+            var data = ev.target.result;
+            state.setData(data);
+            uploadChunk();
         };
         reader.readAsArrayBuffer(file);
     };
